a88a3c0f092789170977d31f82bf9d9997e4febb
shamrin20 <shamrinmillet20@gmail.com>
2023-10-11 13:24:33 +0530

    complete

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -8,6 +8,10 @@ public class ArraySum {
      */
     public int sum(int[] arr){
 
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
     }
 }
